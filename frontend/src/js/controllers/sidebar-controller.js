f85bf800ab0ba2e2ed49187f8ddce7002b0bee2c
Juha Komulainen <juha.komulainen@evident.fi>
2014-05-07 16:43:43 +0300

    Removed external links from sidebar.

--- frontend/src/js/controllers/sidebar-controller.js
+++ frontend/src/js/controllers/sidebar-controller.js
@@ -2,33 +2,9 @@
 
 var controllers = require('angular').module('blogular.controllers');
 
-var externalLinks = [
-    {
-        title: 'Bitbucket',
-        href: 'https://bitbucket.org/evidentsolutions',
-        icon: 'twitter'
-    },
-    {
-        title: 'GitHub',
-        href: 'https://bitbucket.org/evidentsolutions',
-        icon: 'github'
-    },
-    {
-        title: 'Twitter',
-        href: 'https://twitter.com/EvidentSolution',
-        icon: 'twitter'
-    },
-    {
-        title: 'Facebook',
-        href: 'https://www.facebook.com/evidentsolutions',
-        icon: 'facebook'
-    }
-];
-
 controllers.controller('SidebarController', ['$scope', 'postService', ($scope, postService) => {
     var sidebar = $scope.sidebar = {
-        about: '<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>',
-        externalLinks: externalLinks
+        about: '<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>'
     };
 
     function loadPosts() {
