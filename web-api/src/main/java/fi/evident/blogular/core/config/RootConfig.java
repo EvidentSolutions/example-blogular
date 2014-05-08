55483b375f6b448498d34a48289f60a8550c7320
Juha Komulainen <juha.komulainen@evident.fi>
2014-05-08 17:57:43 +0300

    Added basic security configuration.

--- web-api/src/main/java/fi/evident/blogular/core/config/RootConfig.java
+++ web-api/src/main/java/fi/evident/blogular/core/config/RootConfig.java
@@ -1,12 +1,13 @@
 package fi.evident.blogular.core.config;
 
 import fi.evident.blogular.core.config.profiles.ProfilesConfiguration;
+import fi.evident.blogular.security.SecurityConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ProfilesConfiguration.class, DatabaseConfiguration.class, JsonConfiguration.class})
+@Import({ProfilesConfiguration.class, DatabaseConfiguration.class, JsonConfiguration.class, SecurityConfiguration.class})
 @ComponentScan(basePackages = "fi.evident.blogular.core")
 public class RootConfig {
 }
---
+++ web-api/src/main/java/fi/evident/blogular/security/SpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package fi.evident.blogular.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * This class will be automatically loaded by the container and will initialize Spring security.
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
