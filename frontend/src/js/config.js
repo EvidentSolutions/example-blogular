1be602e316ca7150a9f98ef17a1d9ca82dfc54d9
Juha Komulainen <juha.komulainen@evident.fi>
2014-05-08 11:35:36 +0300

    We don't need to import process explicitly. (And it won't even work with new versions of Browserify.)

--- frontend/src/js/config.js
+++ frontend/src/js/config.js
@@ -2,10 +2,6 @@
 
 // This file contains all the things that can be configured using build time.
 // We use envify to replace values of form 'process.env.FOO' by constants.
-
-var process = require('process');
-
-// The environment references here will be replaced at build time by envify.
 module.exports = {
     apiBase: process.env.API_BASE,
     useTemplateCache: process.env.USE_TEMPLATE_CACHE,
