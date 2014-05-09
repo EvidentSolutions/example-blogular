eeb5de30cf4fef50a0c28e631716ea0eef71e770
Juha Komulainen <juha.komulainen@evident.fi>
2014-05-09 16:26:16 +0300

    Remember the login in local-storage so that we don't use it if we open a new tab.

--- frontend/src/js/services/login-service.js
+++ frontend/src/js/services/login-service.js
@@ -7,25 +7,44 @@ var $ = require('jquery');
 var services = angular.module('blogular.services');
 
 services.service('loginService', ['$rootScope', '$http', '$q', ($rootScope, $http, $q) => {
-    $rootScope.currentUser = null;
-
     return {
         login(username, password) {
             var url = config.apiBase + '/account/login';
             var data = $.param({ username: username, password: password });
             var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
             return $http({method: 'POST', url: url, data: data, headers: headers}).then((r) => {
-                return $rootScope.currentUser = r.data;
+                var user = r.data;
+                $rootScope.currentUser = user;
+
+                localStorage['currentUser.name'] = user.name;
+                localStorage['currentUser.authToken'] = user.authToken;
+
+                return user;
             });
         },
 
         logout() {
             $rootScope.currentUser = null;
 
+            delete localStorage['currentUser.name'];
+            delete localStorage['currentUser.authToken'];
+
             var deferred = $q.defer();
             deferred.resolve();
             return deferred.promise;
         }
+    }
+}]);
+
+services.run(['$rootScope', ($rootScope) => {
+    // If we have already logged in, use the credentials from local-storage.
+    if (localStorage['currentUser.name']) {
+        $rootScope.currentUser = {
+            name: localStorage['currentUser.name'],
+            authToken: localStorage['currentUser.authToken']
+        }
+    } else {
+        $rootScope.currentUser = null;
     }
 }]);
 
