36acb08942375739500e35034616d2ac4fa88997
Juha Komulainen <juha.komulainen@evident.fi>
2015-02-10 15:54:13 +0200

    Configure blogular-specific stuff locally.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,4 +1,54 @@
 "use strict";
 
-var settings = require('evident-gulp-build').settings;
+var gulp = require('gulp');
+var revall = require('gulp-rev-all');
+var path = require('path');
+
+var gulpBuild = require('evident-gulp-build');
+var settings = gulpBuild.settings;
+
 settings.indexPagePattern = /^\/(post|posts|login)(\/.*)?$/;
+
+settings.paths.vendorStylesheets = [
+    './bower_components/bootstrap/dist/css/bootstrap.min.css',
+    './bower_components/font-awesome/css/font-awesome.min.css',
+    './bower_components/animate.css/animate.min.css'
+];
+
+// Creates a production build
+gulp.task('build-production', ['clean'], function() {
+    settings.staticBundle = true;
+    gulp.start('build-optimized');
+});
+
+// Creates a development build
+gulp.task('build-development', ['clean'], function() {
+    settings.staticBundle = false;
+    gulp.start('build');
+});
+
+// Create an optimized build
+gulp.task('build-optimized', ['build'], function() {
+    return gulp.src(path.join(settings.paths.output, 'static/**'))
+        .pipe(revall({ ignore: [/^index.html$/, /^css\/epiceditor\/.+/] }))
+        .pipe(gulp.dest(path.join(settings.paths.output, 'optimized')));
+});
+
+gulp.task('styles:copy-extra-resources', ['styles:copy-epic-editor', 'styles:copy-fonts']);
+
+gulp.task('styles:copy-epic-editor', function() {
+    return gulp.src('./bower_components/epiceditor/epiceditor/**/*.css')
+        .pipe(gulp.dest(path.join(settings.paths.output, 'static/css/epiceditor')))
+        .on('error', gulpBuild.errorHandler);
+});
+
+// Copies fonts to proper place
+gulp.task('styles:copy-fonts', function() {
+    var fonts = [
+        './bower_components/bootstrap/dist/fonts/*',
+        './bower_components/font-awesome/fonts/*'
+    ];
+    return gulp.src(fonts)
+        .pipe(gulp.dest(path.join(settings.paths.output, 'static/fonts')))
+        .on('error', gulpBuild.errorHandler);
+});
