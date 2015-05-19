bbcfdb7f3988a869b7cb4e348c5f5e9a41930d5c
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-19 12:44:21 +0300

    Don't import scanned configuration classes manually

--- web-api/src/main/java/fi/evident/blogular/core/config/RootConfig.java
+++ web-api/src/main/java/fi/evident/blogular/core/config/RootConfig.java
@@ -1,13 +1,12 @@
 package fi.evident.blogular.core.config;
 
-import fi.evident.blogular.core.config.profiles.ProfilesConfiguration;
 import fi.evident.blogular.security.SecurityConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ProfilesConfiguration.class, DatabaseConfiguration.class, JsonConfiguration.class, SecurityConfiguration.class})
+@Import(SecurityConfiguration.class)
 @ComponentScan(basePackages = "fi.evident.blogular.core")
 public class RootConfig {
 }
--- web-api/src/main/java/fi/evident/blogular/core/config/profiles/ProfilesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package fi.evident.blogular.core.config.profiles;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import(DevelopmentConfiguration.class)
-public class ProfilesConfiguration {
-}
