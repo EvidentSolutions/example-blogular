9aec54bd3fcc1ddde235e6515290022f4965bba3
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-19 12:45:02 +0300

    Added configuration for standalone mode

---
+++ web-api/src/main/java/fi/evident/blogular/core/config/profiles/StandaloneConfiguration.java
@@ -0,0 +1,18 @@
+package fi.evident.blogular.core.config.profiles;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("standalone")
+public class StandaloneConfiguration {
+
+    @Bean(destroyMethod = "shutdown")
+    public EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).addScript("db/misc/hsql-pg-mode.sql").build();
+    }
+}
